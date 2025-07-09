1056c138379258c4766dee9dd7398f7aa6af0da7
mtuannguyen39 <madserth0309@gmail.com>
2025-07-09 21:02:20 +0700

    News Card fix

--- src/modules/news/ui/components/news-card.tsx
+++ src/modules/news/ui/components/news-card.tsx
@@ -21,14 +21,22 @@ export default function NewsCard({
       <div className={isFeatured ? "aspect-[4/3]" : "aspect-[4/3]"}>
         <Link href={`/news/${id}`}>
           <div className="bg-white shadow rounded overflow-hidden hover:shadow-md transition duration-200 h-full flex flex-col">
-            <Image
-              src={thumbnail}
-              alt={title}
-              width={isFeatured ? 500 : 300}
-              height={isFeatured ? 300 : 200}
-              className="w-full h-full object-cover"
-              sizes={isFeatured ? "(min-width: 1024px) 500px, 100vw" : "300px"}
-            />
+            {thumbnail ? (
+              <Image
+                src={thumbnail}
+                alt={title}
+                width={isFeatured ? 500 : 300}
+                height={isFeatured ? 300 : 200}
+                className="w-full h-full object-cover"
+                sizes={
+                  isFeatured ? "(min-width: 1024px) 500px, 100vw" : "300px"
+                }
+              />
+            ) : (
+              <div className="w-full h-full bg-gray-200 flex items-center justify-center">
+                <span className="text-gray-500">No Image</span>
+              </div>
+            )}
             <div className="p-4 flex-1 flex items-center justify-center">
               <h3 className="text-sm md:text-lg font-semibold text-gray-800 truncate text-center">
                 {title}
