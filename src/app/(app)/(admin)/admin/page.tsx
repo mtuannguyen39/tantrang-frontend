b1d42b8fb97c405a8e7691d75aa0a644b789f3ba
mtuannguyen39 <madserth0309@gmail.com>
2025-08-21 04:14:54 +0700

    [21/8]-tuandev-Force admin user login before using admin dashboard

--- src/app/(app)/(admin)/admin/page.tsx
+++ src/app/(app)/(admin)/admin/page.tsx
@@ -1,5 +1,26 @@
+"use client";
+
 import AdminCard from "@/components/admin/Card";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default async function AdminDashboard() {
-  return <AdminCard />;
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("adminToken");
+    if (!token) {
+      console.log("No admin token found, redirecting to login page");
+      router.push("/admin/login");
+    }
+  }, [router]);
+
+  return (
+    <div className="space-y-6">
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl fond-bold text-gray-900">Dashboard</h1>
+      </div>
+      <AdminCard />
+    </div>
+  );
 }
--- src/components/admin/CreateAdminModal.tsx
+++ src/components/admin/CreateAdminModal.tsx
@@ -52,7 +52,7 @@ export default function CreateAdminModal({
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-gradient-to-tr from-cyan-200 from- via-blue-400 via- to-lime-200 to-50 bg-opacity-50 flex items-center justify-center z-50">
       <div className="bg-white rounded-lg p-6 w-full max-w-md max-h-[90vh] overflow-y-auto">
         <div className="flex items-center justify-between mb-4">
           <h2 className="text-xl font-bold">Tạo Admin mới</h2>
