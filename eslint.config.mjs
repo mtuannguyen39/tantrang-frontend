a14a67bb9d1e114962194f80d64e40111d64dec7
mtuannguyen39 <madserth0309@gmail.com>
2025-08-23 17:29:42 +0700

    Add no-unescaped-entities in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,14 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
+  ...compat.config({
+    extends: ["next"],
     rules: {
-      "@typescript-eslint/no-unused-vars": "off",
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-imports": "off",
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
     },
-  },
+  }),
 ];
 
 export default eslintConfig;
